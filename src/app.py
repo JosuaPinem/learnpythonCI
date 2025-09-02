13fe2ba5e1c22bb2622ac2e37f7945331ec5e07f
Josua Pinem <nusanet@MacBook-Air-Nusanet.local>
2025-09-02 15:26:06 +0700

    Update test and app

--- src/app.py
+++ src/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
 	return "Hello, world!"
 
+@app.route("/perulangan")
+def perulangan():
+	return [i for i in range(1,11)]
+
 if __name__ == "__main__":
 	app.run()
--- test/test_app.py
+++ test/test_app.py
@@ -1,4 +1,8 @@
-from app import index
+from src.app import index
+from src.app import perulangan
 
 def test_index():
 	assert index() == "Hello, world!"
+
+def test_perulangan():
+	assert perulangan() == [1,2,3,4,5,6,7,8,9,10]
